83fb499c08552a8397bf682dc2db5c16666ad832
Uvais Ahmad <uvss21@gmail.com>
2022-12-09 22:49:36 +0530

    created schemas

--- models/orders.js
+++ models/orders.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
+    dateTime : {
+        type : Date,
+        default : Date.now()
+    },
     totalFee : {
         type : String,
         required : true
--- router/index.js
+++ router/index.js
@@ -2,4 +2,7 @@ const express = require('express');
 const router = express.Router();
 console.log('Router file readen');
 
+router.use('/order',require('./order'));
+router.use('/service',require('./service'));
+
 module.exports = router;
